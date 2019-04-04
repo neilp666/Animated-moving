04d30fe533202dc764f308564531e54ca479a1b4
neil patel <neil@myindianweddingapp.com>
2019-04-04 11:36:40 +0100

    Added final animation and hearbeat keyframe

--- ball.js
+++ ball.js
@@ -3,11 +3,24 @@ const ball = document.querySelector('div.ball')
 let mouseX = 0
 let mouseY = 0
 
+let ballX = 0
+let ballY = 0
+
+let speed = 0.02
+
 function animate() {
-    ball.style.left = mouseX + 'px'
-    ball.style.top = mouseY + 'px'   
+
+    let distX = mouseX - ballX
+    let distY = mouseY - ballY
+
+    ballX = ballX + (distX * speed)
+    ballY = ballY + (distY * speed)
+
+    ball.style.left = ballX + 'px'
+    ball.style.top = ballY + 'px'   
 
     requestAnimationFrame(animate)
+    
 }
 
 animate()
